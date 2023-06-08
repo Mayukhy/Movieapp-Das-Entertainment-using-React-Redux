b7889c8279699e8c45adeffe95b34bb29a330b64
Mayukh Das <107027766+Mayukhy@users.noreply.github.com>
2023-06-08 21:46:55 +0530

    Update Api.js

--- src/utls/Api.js
+++ src/utls/Api.js
@@ -1,7 +1,8 @@
+//TMDB api is used for getting Movie Data
 import React from 'react'
 import axios from "axios";
 const BASE_URL='https://api.themoviedb.org/3';
-const TMBD_TOKEN ='eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZmM4MDlkOTU0MmEzNzM2Yjk5MDE5MDQ1YzFjOTBhYyIsInN1YiI6IjY0NTQ3ODdiODdhMjdhMDE3MjNkMWNmOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ny-nEq07gMlCUnhQiFTiJuouIUXXJQwahknSzVJBSRs';
+const TMBD_TOKEN ='use own TMDB Token';
 const headers={
  Authorization: "bearer " + TMBD_TOKEN  
 }
